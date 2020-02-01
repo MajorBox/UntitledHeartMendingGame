d61d123a4951126cad3c2df63727804e5002a965
Alvaro Sebastián Galindo Calderón <sebastian28.gc@gmail.com>
2020-02-01 17:13:06 -0600

    Added timer and collectibles

---
+++ UnityProject/UntitledHeartMendingGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotateVector = new Vector3(15, 0, 0); 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateVector * Time.deltaTime);
+    }
+}
