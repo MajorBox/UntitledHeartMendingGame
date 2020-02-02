fb0474641eaf3299969e7084ccd3453d828d9471
Alvaro Sebastián Galindo Calderón <sebastian28.gc@gmail.com>
2020-02-02 11:44:03 -0600

    Added audio assets and sprites

--- UnityProject/UntitledHeartMendingGame/Assets/Scripts/Collectible.cs
+++ UnityProject/UntitledHeartMendingGame/Assets/Scripts/Collectible.cs
@@ -4,12 +4,25 @@ public class Collectible : MonoBehaviour
 {
     public GameManager gameManager;
 
+    void Start()
+    {
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
+            if(gameManager.collectibleCount <= 2)
+            {
+                AudioManager.Instance.PlayCollectible();
+            }
+            else
+            {
+                AudioManager.Instance.PlayVictory();
+            }
+            
             gameManager.collectibleCount = gameManager.collectibleCount + 1;
-            this.gameObject.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
