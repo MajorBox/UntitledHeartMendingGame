82b16c66a1e6848ca953a0f798abc7e8e1af63f2
Rod <rodrigo.ramirez.sc@gmail.com>
2020-02-02 16:16:13 -0600

    Kokoro headeaches

---
+++ UnityProject/UntitledHeartMendingGame/Assets/PlayParticles_Get.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticles_Get : MonoBehaviour
+{
+    public ParticleSystem KokoroGet;
+
+    private void Awake()
+    {
+        KokoroGet = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player found");
+            KokoroGet.Play();
+        }
+    }
+
+}
--- UnityProject/UntitledHeartMendingGame/Assets/Scripts/Collectible.cs
+++ UnityProject/UntitledHeartMendingGame/Assets/Scripts/Collectible.cs
@@ -3,9 +3,15 @@
 public class Collectible : MonoBehaviour
 {
     public GameManager gameManager;
+    public BoxCollider2D box_collider;
+    public GameObject PS_Kokoro;
+    public Transform GO_Transf;
+
 
     void Start()
     {
+        box_collider = GetComponent<BoxCollider2D>();
+        GO_Transf = gameObject.GetComponent<Transform>();
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -22,7 +28,10 @@ public class Collectible : MonoBehaviour
             }
             
             gameManager.collectibleCount = gameManager.collectibleCount + 1;
+            Instantiate(PS_Kokoro,GO_Transf);
             gameObject.SetActive(false);
+
+
         }
     }
 }
